88188dd79149df7bf61253d084410dfb9d929a55
Jack Tansley <35172812+thisjacktansley@users.noreply.github.com>
2018-01-06 16:39:11 +0000

    Create rps.js

---
+++ cmds/rps.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js')
+
+exports.run = (bot, message, args) => {
+if 
+}
+
+exports.help = {
+name: "NAME OF COMMAND",
+description: "DESCRIPTION OF COMMAND",
+usage: "USAGE OF COMMAND"
+}
+
+exports.aliases = ["This is how", "You make aliases", "Dont include the prefix though"
