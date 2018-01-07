e4610a2bf9c4d3c15565d5c89b7cbfbde0e912e2
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2018-01-07 16:15:05 -0600

    Update np.js

--- cmds/np.js
+++ cmds/np.js
@@ -11,10 +11,10 @@ exports.run = async(bot, message, args, queue) => {
   const url = args1[1] ? args1[1].replace(/<(.+)>/g, '$1') : '';
   const serverQueue = queue.get(message.guild.id);
   
-      const voiceChannel = message.member.voiceChannel
+     const voiceChannel = message.member.voiceChannel
 
   if (message.channel.id !== "399548148315586580") return message.channel.send(":musical_note: :x: ***__You need to be in <#399548148315586580> To use this command!__*** :x: :musical_note:")
-  if (message.member.voiceChannelID !== "399540529995710484" && !message.member.voiceChannelID) return message.channel.send(":musical_note: :x:***__You Must be in the music room to use this Command!__*** :x: :musical_note:")
+  if (message.member.voiceChannelID !== "399540529995710484") return message.channel.send(":musical_note: :x:***__You Must be in the music room to use this Command!__*** :x: :musical_note:")
   
 
     if (!serverQueue) return message.channel.send("There isn't anything playing");
