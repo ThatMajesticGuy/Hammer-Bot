3aed5775bdaad86f189ac1562e2e61d9ab4b565e
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2018-01-07 16:03:28 -0600

    Update queue.js

--- cmds/queue.js
+++ cmds/queue.js
@@ -12,10 +12,11 @@ exports.run = async(bot, message, args, queue) => {
   const serverQueue = queue.get(message.guild.id);
 
 
-    const voiceChannel = message.member.voiceChannelID
+    const voiceChannel = message.member.voiceChannel
 
   if (message.channel.id !== "399548148315586580") return message.channel.send(":musical_note: :x: ***__You need to be in <#399548148315586580> To use this command!__*** :x: :musical_note:")
-  if (voiceChannel !== "399540529995710484" && !voiceChannel) return message.channel.send(":musical_note: :x:***__You Must be in the music room to use this Command!__*** :x: :musical_note:")
+  if (message.member.voiceChannelID !== "399540529995710484" && !message.member.voiceChannelID) return message.channel.send(":musical_note: :x:***__You Must be in the music room to use this Command!__*** :x: :musical_note:")
+     
     if (!serverQueue) return message.channel.send('There is nothing playing! Add some music to play using: h!play [song-name]');
     return message.channel.send(`
 __**Song queue:**__
