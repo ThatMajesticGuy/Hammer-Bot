323c95d65d2f3c64e5aa365f61077dab3fe7312b
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2018-01-07 16:08:50 -0600

    Update skip.js

--- cmds/skip.js
+++ cmds/skip.js
@@ -17,7 +17,7 @@ exports.run = async(bot, message, args, queue) => {
     const voiceChannel = message.member.voiceChannel
 
   if (message.channel.id !== "399548148315586580") return message.channel.send(":musical_note: :x: ***__You need to be in <#399548148315586580> To use this command!__*** :x: :musical_note:")
-  if (message.member.voiceChannelID !== "399540529995710484" && !message.member.voiceChannelID) return message.channel.send(":musical_note: :x:***__You Must be in the music room to use this Command!__*** :x: :musical_note:")
+  if (message.member.voiceChannelID !== "399540529995710484") return message.channel.send(":musical_note: :x:***__You Must be in the music room to use this Command!__*** :x: :musical_note:")
 	
 		if (!serverQueue) return message.channel.send("I can't skip Nothing?");
       		serverQueue.connection.dispatcher.end('Skip command has been used!');
