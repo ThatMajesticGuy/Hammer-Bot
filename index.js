fd8529380f8274c2a0b03a4719795bfa496f3248
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2018-01-09 17:56:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,101 @@
-bot.on("message", message => {
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const prefix = "h!";
+const dateformat = require('dateformat');
+const fs = require('fs');
+const queue = new Map();
+
+bot.login(process.env.BOT_TOKEN);
+
+bot.on('ready', ready => {
+  console.log("Ready to serve the hammer kingdom!")
+  bot.user.setGame(`with ${bot.users.size} people!`)
+});
+
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+fs.readdir("./cmds/", (err, files) => {
+    if(err) console.error((err));
+    let jsfiles = files.filter(f => f.split(".").pop() === "js");
+    if(jsfiles.length <= 0) {
+        console.log("Do you mind making the commands first?".red);
+        return;
+    }
+
+
+    console.log(`Loading ${jsfiles.length} commands!`);
+
+    jsfiles.forEach((f, i) => {
+        delete require.cache[require.resolve(`./cmds/${f}`)]
+        let props = require(`./cmds/${f}`)
+        console.log(`${i + 1}: ${f} loaded!`);
+        bot.commands.set(props.help.name, props);
+        props.aliases.forEach(alias => {
+        bot.aliases.set(alias, props.help.name);
+    });
+
+    });
+ });
+
+ bot.on("message", async message => {
+    if(message.author.bot) return;
+
+     if(message.channel.type === "dm") {
+        var embed = new Discord.RichEmbed()
+        .setTitle("ERROR!")
+        .setColor("RANDOM")
+        .setThumbnail(`${message.author.displayAvatarURL}`)
+        .setTimestamp()
+        .addField("ERROR!", "I currently don't work in DMs")
+         message.channel.send({ embed: embed })
+         return;
+    }
+
+    let messageArray = message.content.split(/\s+/g);
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(!command.startsWith(prefix)) return;
+
+    if (bot.aliases.has(command.slice(prefix.length).toLowerCase())) {
+        bot.commands.get(bot.aliases.get(command.slice(prefix.length).toLowerCase())).run(bot, message, args, queue)
+    }
+        if (bot.commands.has(command.slice(prefix.length).toLowerCase())) {
+         bot.commands.get(command.slice(prefix.length).toLowerCase()).run(bot, message, args, queue)
+        }
+
+});
+
+bot.on("guildMemberAdd", member => {
+    let avatar;
+  if (!member.user.avatarURL) avatar = member.guild.iconURL;
+  if (member.user.avatarURL) avatar = member.user.avatarURL;
+  const log = bot.channels.get("399228331834212354")
+  var embed = new Discord.RichEmbed()
+  .setTitle("Welcome <:blobwave:399237241035030538>")
+  .setColor("#2bc237")
+  .setTimestamp()
+  .setThumbnail(avatar)
+  .setAuthor("Hammer Kingdom", member.guild.iconURL)
+  .addField(`:bust_in_silhouette: Ayyy its __${member.user.username}__! :bust_in_silhouette:`, ":wave: Welcome to the **__Hammer Kingdom!__** :wave:")
+  .addField("Make sure to **__read the rules__**", "And have fun! <:owo:317007752465743872>")
+log.send({ embed: embed })
+})
+
+bot.on("guildMemberRemove", member => {
+      const log = bot.channels.get("399228344719114251")
+      var embed = new Discord.RichEmbed()
+      .setTitle("Goodbye <:down:317008439316578314>")
+      .setColor("BLUE")
+      .setTimestamp()
+      .setThumbnail(member.user.displayAvatarURL)
+      .setAuthor("Hammer Kingdom", member.guild.iconURL)
+      .addField(`<:weep:317012456327348225> __${member.user.username}__ has left the server... <:weep:317012456327348225>`, "Lets hope we see them soon!")
+      log.send({ embed: embed })
+});
+
+bot.on("message", async message => {
 	if (message.author.bot) return
 	if (!message.content.startsWith(prefix) return;
 
@@ -25,8 +122,8 @@ bot.on("message", message => {
 			const playlist = await youtube.getPlaylist(url);
 			const videos = await playlist.getVideos();
 			for (const video of Object.values(videos)) {
-				const video2 = await youtube.getVideoByID(video.id);
-				await handleVideo(video2, message, voiceChannel, true); 
+				const video2 = await youtube.getVideoByID(video.id); // eslint-disable-line no-await-in-loop
+				await handleVideo(video2, message, voiceChannel, true); // eslint-disable-line no-await-in-loop
 			}
 			return message.channel.send(`✅ Playlist: **${playlist.title}** has been added to the queue!`);
 		} else {
@@ -41,7 +138,7 @@ __**Song selection:**__
 ${videos.map(video2 => `**${++index} -** ${video2.title}`).join('\n')}
 Please provide a value to select one of the search results ranging from 1-10.
 					`);
-		
+					// eslint-disable-next-line max-depth
 					try {
 						var response = await message.channel.awaitMessages(message2 => message2.content > 0 && message2.content < 11, {
 							maxMatches: 1,
@@ -151,9 +248,19 @@ function play(guild, song) {
 	const serverQueue = queue.get(guild.id);
 
 	if (!song) {
-		serverQueue.voiceChannel.leave();
-		queue.delete(guild.id);
-		return;
+	const playlist = await youtube.getPlaylist(url);
+	const videos = await playlist.getVideos();
+	for (const video of Object.values(videos)) {
+	const video2 = await youtube.getVideoByID(video.id);
+	const dispatcher = serverQueue.connection.playStream(ytdl(video2.url))
+			.on('end', reason => {
+			if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
+			else console.log(reason);
+			serverQueue.songs.shift();
+			play(guild, serverQueue.songs[0]);
+		})
+		.on('error', error => console.error(error));
+	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
 	}
 	console.log(serverQueue.songs);
 
@@ -190,3 +297,17 @@ if(song.durationh !== 0) {
     if(secondslength !== 1 || secondslength !== 0) {
     return serverQueue.textChannel.send(`🎶 Now playing: **${song.title}** (${song.durationh}:${song.durationm}:${durations})`);
 }}}
+if(song.durationh == 0 && song.durationm !== 0) {
+  if(secondslength == 1 || secondslength == 0) {
+    return serverQueue.textChannel.send(`🎶 Now playing: **${song.title}** (${song.durationm}:0${durations})`);
+}}
+if(song.durationh == 0 && song.durationm !== 0) {
+  if(secondslength !== 1 || secondslength !== 0) {
+    return serverQueue.textChannel.send(`🎶 Now playing: **${song.title}** (${song.durationm}:${durations})`);
+}}
+if(song.durationh == 0 && song.durationm == 0 && song.durations !== 0) {
+  return serverQueue.textChannel.send(`🎶 Now playing: **${song.title}** (${durations} Seconds)`);
+} else {
+  return serverQueue.textChannel.send(`🎶 Now playing: **${song.title}**`);
+}
+}
